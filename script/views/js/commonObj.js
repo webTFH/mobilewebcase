115a19636119585c0d7c64c4069c696bc6e12a91
liuzijie <fred.liu@ximalaya.com>
2017-10-30 22:35:30 +0800

    fix

--- script/views/js/commonObj.js
+++ script/views/js/commonObj.js
@@ -4,7 +4,9 @@ contentMerch = '<meta charset="UTF-8">\
     <p class="ptitle">%merchName%</p>\
     <canvas data-src="%coverPath%" ></canvas>\
 	<div style="bottom:20px;position: relative;">价格:<span style="color: #ff7301;background-color: white">%price%￥</span></div>\
-    <p class="bl" style="bottom:18px;position: relative"><span class="green" >%rowId%</span></p>\
+    <p class="bl" style="bottom:18px;position: relative">\
+    	<span class="green" style="color: #e54144">货道</span>\
+    	<span class="green" >%rowId%</span></p>\
     </div>\
     </a>';
 pagenumberGlobla = 0;
